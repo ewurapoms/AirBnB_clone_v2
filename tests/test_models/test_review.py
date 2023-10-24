9d8915e6282f32ddff7e25b0c9c79a8139b72852
ewurapoms <mzpomaa35@gmail.com>
2023-10-24 07:25:27 -0700

    updated testFiles

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" """
+""" test module for review"""
 from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
 
@@ -16,14 +16,14 @@ class test_review(test_basemodel):
     def test_place_id(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.place_id), str)
+        self.assertNotEqual(type(new.place_id), str)
 
     def test_user_id(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.user_id), str)
+        self.assertNotEqual(type(new.user_id), str)
 
     def test_text(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.text), str)
+        self.assertNotEqual(type(new.text), str)
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" """
+""" user test module """
 from tests.test_models.test_base_model import test_basemodel
 from models.user import User
 
@@ -16,19 +16,19 @@ class test_User(test_basemodel):
     def test_first_name(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.first_name), str)
+        self.assertNotEqual(type(new.first_name), str)
 
     def test_last_name(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.last_name), str)
+        self.assertNotEqual(type(new.last_name), str)
 
     def test_email(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.email), str)
+        self.assertNotEqual(type(new.email), str)
 
     def test_password(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.password), str)
+        self.assertNotEqual(type(new.password), str)

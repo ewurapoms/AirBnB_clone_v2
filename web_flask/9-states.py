e7160f4751c01029373de5b4d86239af8263b565
ewurapoms <mzpomaa35@gmail.com>
2023-11-25 07:49:11 -0800

    new updates

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -10,28 +10,27 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route('/states')
-def states():
-    """shows an HTML page that lists all states & cities"""
-    states = storage.all(State).values()
-    newStates = sorted(states, key=lambda s: s.name)
-    return render_template('9-states.html', state=newStates)
-
-
-@app.route('/states/<id>')
-def states_id(id):
-    """prints an HTML about <id>, where existing"""
-    for state in storage.all(State).values():
-        if state.id == id:
-            return render_template('9-states.html', state=state)
-    return render_template('9-states.html')
-
-
 @app.teardown_appcontext
 def teardown(self):
     """ends database session"""
     storage.close()
 
 
+@app.route('/states')
+def states():
+    """shows an HTML page that lists all states & cities"""
+    states = storage.all(State)
+    return render_template('9-states.html', state=states)
+
+
+@app.route('/states/<id>')
+def states_id(id):
+    """prints an HTML about <id>, where existing"""
+    for state in storage.all(State):
+        if state.id == id:
+            return render_template('9-states.html', state=state)
+    return render_template('9-states.html')
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)

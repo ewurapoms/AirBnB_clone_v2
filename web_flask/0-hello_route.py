b1475f52f737098588d78e12a4091d2e6cb11ac2
ewurapoms <mzpomaa35@gmail.com>
2023-11-24 23:35:23 -0800

    script that starts a Flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+Web application must be listening on 0.0.0.0, port 5000
+Routes:
+/: display "Hello HBNB!"
+Must include: strict_slashes=False in route def
+"""
+#installation : pip3 install Flask
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route('/')
+def hello_hbnb():
+    """
+    strict_slashes set to False enables the
+    URL maintain functionality with or without 
+    the / symbol at the end during routing
+    """
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)

04923e3393c739d032b3ae3dd02f979f92d3f43c
ewurapoms <mzpomaa35@gmail.com>
2023-11-25 02:30:28 -0800

    Flask web application: Lists all States

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Start a Flask web application:state list"""
+
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/states_list')
+def states_list():
+    """printss an HTML page that lists of all states"""
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def db_teardown(self):
+    """ends any current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)

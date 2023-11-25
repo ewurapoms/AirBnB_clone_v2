a6de43fad40b985f7a66d10509c294cfe7fee10c
ewurapoms <mzpomaa35@gmail.com>
2023-11-25 00:22:06 -0800

    script starts Flask Web App: Python is cool!

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Starts a Flask web applicaton"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    """
+    prints the return value
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """
+    prints the HBNB text
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """
+    prints a C, then the value of the text variable
+    """
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python_is_cool(text='is cool'):
+    """ Prints 'Python', then value of the text variable"""
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)

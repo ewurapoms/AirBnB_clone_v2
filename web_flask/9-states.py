a694be834c07ec34407a5db181af52a03e9656cb
ewurapoms <mzpomaa35@gmail.com>
2023-11-25 06:32:55 -0800

    removed .values() from states function

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -2,35 +2,34 @@
 """Start a Flask web application:States and State"""
 
 from flask import Flask, render_template
-from models.city import City
 from models import storage
 from models.state import State
-
+from models.city import City
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.teardown_appcontext
-def teardown(self):
-    """ends database session"""
-    storage.close()
-
-
 @app.route('/states')
 def states():
     """shows an HTML page that lists all states & cities"""
-    states = storage.all(State).values()
+    states = storage.all(State)
     return render_template('9-states.html', state=states)
 
 
-@app.route("/states/<id>")
+@app.route('/states/<id>')
 def states_id(id):
     """prints an HTML about <id>, where existing"""
     for state in storage.all(State).values():
         if state.id == id:
             return render_template("9-states.html", state=state)
     return render_template("9-states.html")
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """ends database session"""
+    storage.close()
 
 
 if __name__ == "__main__":

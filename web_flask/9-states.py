e4a5a29b735f62be8c300c2d176e2d6134e680ed
ewurapoms <mzpomaa35@gmail.com>
2023-11-25 07:40:50 -0800

    new updates

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -13,8 +13,9 @@ app.url_map.strict_slashes = False
 @app.route('/states')
 def states():
     """shows an HTML page that lists all states & cities"""
-    states = storage.all(State)
-    return render_template('9-states.html', state=states)
+    states = storage.all(State).values()
+    newStates = sorted(states, key=lambda s: s.name)
+    return render_template('9-states.html', state=newStates)
 
 
 @app.route('/states/<id>')
@@ -33,4 +34,4 @@ def teardown(self):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')

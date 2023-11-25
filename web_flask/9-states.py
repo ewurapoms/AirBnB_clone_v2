fda4c47366334d40b909a098a34e6d719dcd2309
ewurapoms <mzpomaa35@gmail.com>
2023-11-25 05:48:55 -0800

    Flask web application:States and State

---
+++ web_flask/9-states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Start a Flask web application:States and State"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """ends any current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route('/states')
+def states():
+    """shows an HTML page that lists all states & cities"""
+    states = storage.all(State).values()
+    return render_template('9-states.html', state=states)
+
+
+@app.route("/states/<id>")
+def states_id(id):
+     """prints an HTML about <id>, where existing"""
+    for state in storage.all(State).values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)

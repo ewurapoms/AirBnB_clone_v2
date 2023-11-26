03094315a9c82d1b7e266d81d49052072fd075b6
ewurapoms <mzpomaa35@gmail.com>
2023-11-25 22:41:40 -0800

    fixing code

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,35 +1,42 @@
 #!/usr/bin/python3
-"""Starts a Flask web application: HBNB filters"""
+"""
+Starts a Flask web application:
+-listens on 0.0.0.0 and port 5000.
+-routes: /states and /states/<id>
 
-from flask import Flask, render_template
+"""
 from models import storage
-from models.state import State
-
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown(self):
-    """ends current SQLAlchemy Session"""
-    storage.close()
-
-
-@app.route('/states', strict_slashes=False)
+@app.route("/states", strict_slashes=False)
 def states():
-    """shows HTML page with list of all states & cities"""
+    """Shows an HTML page presenting a list of all states,
+    sorted alphabetically by name.
+    """
     states = storage.all("State")
     return render_template("9-states.html", state=states)
 
 
 @app.route("/states/<id>", strict_slashes=False)
 def states_id(id):
-    """prints HTML page with info about <id>, if it exists."""
+    """
+Shows an HTML page with information about <id> if it is present."""
     for state in storage.all("State").values():
         if state.id == id:
             return render_template("9-states.html", state=state)
     return render_template("9-states.html")
 
 
+@app.teardown_appcontext
+def teardown(exc):
+    """
+Removes the existing SQLAlchemy session.."""
+    storage.close()
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0")

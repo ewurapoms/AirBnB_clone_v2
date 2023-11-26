0d9715bb8cad7e760203ad39536252433fd43df1
ewurapoms <mzpomaa35@gmail.com>
2023-11-25 22:36:11 -0800

    fixing code

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -11,21 +11,21 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def teardown(self):
-    """removes the current SQLAlchemy Session"""
+    """ends current SQLAlchemy Session"""
     storage.close()
 
 
 @app.route('/states', strict_slashes=False)
 def states():
-    """Displays a HTML page with a list of all states & cities"""
-    states = storage.all(State)
-    return render_template('9-states.html', state=states)
+    """shows HTML page with list of all states & cities"""
+    states = storage.all("State")
+    return render_template("9-states.html", state=states)
 
 
 @app.route("/states/<id>", strict_slashes=False)
 def states_id(id):
-    """Displays an HTML page with info about <id>, if it exists."""
-    for state in storage.all(State).values():
+    """prints HTML page with info about <id>, if it exists."""
+    for state in storage.all("State").values():
         if state.id == id:
             return render_template("9-states.html", state=state)
     return render_template("9-states.html")

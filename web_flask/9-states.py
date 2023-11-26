31400df4a09116195486bb956847bac3ee73af06
ewurapoms <mzpomaa35@gmail.com>
2023-11-26 00:01:56 -0800

    script updated

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
-"""Start a Flask web application:Cities by states"""
+"""Start a Flask web application: States by State"""
 
-from flask import Flask, render_template
 from models import storage
-from models.city import City
+from flask import Flask
+from flask import render_template
 from models.state import State
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 @app.route('/states')
-def states_by_state():
-    """shows an HTML page that lists all states """
-    states = storage.all(State).values()
-    return render_template('9-states.html', states=states)
+def states():
+    """prints HTML page with a list of all states & cities"""
+    states = storage.all('State')
+    return render_template('9-states.html', state=states)
 
 
-@app.route("/states/<id>")
+@app.route('/states/<id>')
 def states_id(id):
-    """ shows an HTML page with info about <id>, if it exists """
-    for state in storage.all(State).values():
+    """shows an HTML page with info about <id>, if it exists."""
+    for state in storage.all('State').values():
         if state.id == id:
             return render_template('9-states.html', state=state)
     return render_template('9-states.html')
@@ -33,5 +34,4 @@ def teardown(self):
 
 
 if __name__ == "__main__":
-    storage.reload()
     app.run(host='0.0.0.0', port=5000)

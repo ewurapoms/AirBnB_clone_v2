a0de6bd67eefb8c514facd98d052765b22c94133
ewurapoms <mzpomaa35@gmail.com>
2023-11-25 23:22:24 -0800

    debugging

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Starts a Flask web application"""
+"""Start a Flask web application"""
 
 from flask import Flask, render_template
 from models import storage
@@ -11,20 +11,20 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def teardown(self):
-    """ends SQLAlchemy Session"""
+    """removes the current SQLAlchemy Session"""
     storage.close()
 
 
 @app.route('/states', strict_slashes=False)
 def states():
-    """prints HTML page with a list of all states & cities"""
+    """Displays a HTML page with a list of all states & cities"""
     states = storage.all(State)
     return render_template('9-states.html', state=states)
 
 
 @app.route("/states/<id>", strict_slashes=False)
 def states_id(id):
-    """ shows an HTML page with info about <id>, if it exists """
+    """Displays an HTML page with info about <id>, if it exists."""
     for state in storage.all(State).values():
         if state.id == id:
             return render_template("9-states.html", state=state)

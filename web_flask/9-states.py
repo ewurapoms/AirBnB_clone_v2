5c4c917b402063498bf44cc213553c36d91fc955
ewurapoms <mzpomaa35@gmail.com>
2023-11-25 23:32:06 -0800

    updates

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,35 +1,42 @@
 #!/usr/bin/python3
-"""Start a Flask web application"""
-
-from flask import Flask, render_template
+"""Initiates a Flask web application.
+The application is set to listen on 0.0.0.0, port 5000.
+Routes:
+/states: Generates an HTML page featuring a list of all State objects.
+/states/<id>: Creates an HTML page showcasing the specified state with <id>.
+"""
 from models import storage
-from models.state import State
-
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown(self):
-    """removes the current SQLAlchemy Session"""
-    storage.close()
-
-
-@app.route('/states', strict_slashes=False)
+@app.route("/states", strict_slashes=False)
 def states():
-    """Displays a HTML page with a list of all states & cities"""
-    states = storage.all(State)
-    return render_template('9-states.html', state=states)
+    """Shows an HTML page presenting a list of all states,
+    sorted alphabetically by name.
+    """
+    states = storage.all("State")
+    return render_template("9-states.html", state=states)
 
 
 @app.route("/states/<id>", strict_slashes=False)
 def states_id(id):
-    """Displays an HTML page with info about <id>, if it exists."""
-    for state in storage.all(State).values():
+    """
+Shows an HTML page with information about <id> if it is present."""
+    for state in storage.all("State").values():
         if state.id == id:
             return render_template("9-states.html", state=state)
     return render_template("9-states.html")
 
 
+@app.teardown_appcontext
+def teardown(exc):
+    """
+Removes the existing SQLAlchemy session.."""
+    storage.close()
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0")
